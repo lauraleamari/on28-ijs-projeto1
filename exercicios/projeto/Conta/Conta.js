764aba953260b1d547ed4a5405c0f002317f0cd2
Laura Leamari <laura.leamari@infracommerce.com.br>
2023-11-01 12:39:11 -0300

    aulas semana 07

--- exercicios/projeto/Conta/Conta.js
+++ exercicios/projeto/Conta/Conta.js
@@ -1,39 +1,26 @@
 class Conta{
-  #agencia
-  #conta
-  #saldo
-  chavePix
+  #agencia;
+  #conta;
+  #saldo;
+  chavesPix;
+  static listaContas = [];
 
-  constructor(agencia, conta, saldo, chavePix){
-    this.#agencia = agencia;
-    this.#conta = conta;
-    this.#saldo = saldo;
-    this.chavePix = {
-      cpf: undefined,
-      email: undefined,
-      telefone: undefined
-    }
+  constructor(agencia, conta, saldo){
+      this.#agencia = agencia;
+      this.#conta = conta;
+      this.#saldo = saldo;
+      this.chavesPix = {
+          cpf: undefined,
+          email: undefined,
+          telefone: undefined
+      };
+
+      Conta.listaContas.push(this)
   }
 
-  getAgencia(){
-    return this.#agencia
-  }
-  getConta(){
-    return this.#conta
-  }
-  getSaldo(){
-    return this.#saldo
-  }
+  // Meu jeito de construir
 
-  // setAgencia(newAgencia){
-  //   this.#agencia = newAgencia
-  // }
-
-  setSaldo(novoSaldo){
-    this.#saldo = novoSaldo
-  }
-
-  // criarConta(agencia, conta, saldo) {
+// criarConta(agencia, conta, saldo) {
   //   if (typeof agencia !== 'string' && agencia.length !== 4) {
   //     throw new Error('Dados inválidos para cadastro');
   //   }
@@ -74,48 +61,131 @@ class Conta{
   //   }
   // }
 
-  //Profa
+  // Profa
 
-  criarConta(agencia, conta, saldo) {
-    if(agencia.length === 4 && conta.length === 5 && saldo >= 0){
-      this.#agencia = agencia;
-      this.#conta = conta;
-      this.#saldo = saldo;
-      return "Conta criada com sucesso";
-    } else {
-      throw new Error("Dados inválidos para cadastro")
-    }
+  //metodo para destruir objeto da lista de contas pra salvar sua memoria
+  destruir(){
+      let i = Conta.listaContas.indexOf(this);
+      Conta.listaContas.splice(i, 1)
   }
 
-  sacar(valor){
-    if(valor > 0 && typeof valor === 'number'){
-      if(this.#saldo - valor > 0){
-        const saldoAtualizado = this.#saldo - valor
-        this.setSaldo(saldoAtualizado)
+  criarConta(agencia, conta, saldo){
+      if(agencia.length === 4 && conta.length === 5 && saldo > 0){
+          this.#agencia = agencia;
+          this.#conta = conta;
+          this.#saldo = saldo;
+
+          return "Conta criada com sucesso";
       } else {
-        throw new Error("Saldo insuficiente");
+           throw new Error("Dados inválidos para cadastro");
       }
-    } else {
-      throw new Error("Valor inválido para Saque");
-    }
+
+  }
+  sacar(valor){
+      if(valor > 0 && typeof valor === "number"){
+          if(this.#saldo - valor > 0){
+              const saldoAtualizado = this.#saldo - valor;
+              this.setSaldo(saldoAtualizado)
+          } else {
+              throw new Error("Saldo insuficiente")
+          }
+      } else{
+          throw new Error("Valor inválido para Saque")
+      }
   }
 
   depositar(valor){
-    if(valor > 0 && typeof valor === 'number') {
-      if(this.#saldo + valor > 0){
-        const saldoAtualizado = this.#saldo + valor
-        this.setSaldo(saldoAtualizado)
-
-      } else {
-        throw new Error("Valor inválido para depósito")
+      if(valor > 0 && typeof valor === "number"){
+          const saldoAtualizado = this.#saldo + valor;
+          this.setSaldo(saldoAtualizado)
+      }else{
+          throw new Error("Valor inválido para depósito")
       }
-
-    } else {
-      throw new Error("Valor inválido para depósito");
-    }
   }
 
-}
+  transferir(valor, agencia, conta){
+      //LISTA.find(APELIDO PARA ITEM SELECIONADA => COMPARACAO )
+      /**
+       * antes de fazer a transferencia preciso verificar se a conta receptora existe na lista de contas
+       * contaValida vai me retornar a conta se ela existir e undefined se não existir
+       */
+      let contaValida = Conta.listaContas.find( contaReceptora => {
+          let numeroContaReceptora = contaReceptora.getConta();
+          let numeroAgenciaReceptora = contaReceptora.getAgencia();
+          return numeroContaReceptora === conta && numeroAgenciaReceptora === agencia;
+      })
 
+      if(!contaValida){
+          throw new Error("Conta não encontrada")
+      }
+
+      if(valor < 0){
+          throw new Error("Valor inválido para transferencia")
+      }
+
+      //a conta não pode ficar negativa ao fazer a transferencia,
+      if(this.#saldo - valor > 0){
+          const saldoAtualizado = this.#saldo - valor;
+          this.setSaldo(saldoAtualizado);
+          const saldoContaReceptora = contaValida.getSaldo() + valor
+          contaValida.setSaldo(saldoContaReceptora);
+          return "Tranferencia realizada"
+      }
+  }
+
+  getAgencia(){
+      return this.#agencia;
+  }
+
+  getConta(){
+      return this.#conta;
+  }
+
+  getSaldo(){
+      return this.#saldo;
+  }
+
+  setSaldo(novoSaldo){
+      this.#saldo = novoSaldo;
+  }
+
+  criarChavePix(chavePix, tipo){
+      switch (tipo) {
+          case "CPF":
+              let regexCPF = /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/;
+              if (regexCPF.test(chavePix)) {
+                  this.chavesPix.cpf = chavePix
+                  return "Chave Pix por cpf criada com sucesso"
+              } else {
+                  throw new Error("Erro: CPF inválido");
+              }
+
+          case "EMAIL":
+              let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+              if (regexEmail.test(chavePix)) {
+                  this.chavesPix.email = chavePix
+                  return "Chave Pix por email criada com sucesso"
+              } else {
+                  throw new Error("Erro: Email inválido");
+              }
+
+          case "TELEFONE":
+              let regexTelefone = /^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$/;
+              if (regexTelefone.test(chavePix)) {
+                  this.chavesPix.telefone = chavePix
+                  return "Chave Pix por telefone criada com sucesso"
+              } else {
+                  throw new Error("Erro: Telefone inválido");
+              }
+
+          default:
+              return "Chave inexistente"
+
+      }
+
+  }
+
+
+}
 
 module.exports = Conta
--- exercicios/projeto/Conta/Conta.test.js
+++ exercicios/projeto/Conta/Conta.test.js
@@ -1,3 +1,13 @@
+/*
+ESTRUTURA DOS TESTES
+test("DESCRIÇÃO", () => {
+    // assim que se começa um teste!
+    // setup (o que a função/método/classe que vai ser testada precisa para funcionar?)
+    // acao (execução da função testada)
+    // verificacao (o que eu quero que seja retornado , comparado com o que de fato é retornado)
+  })
+*/
+
 const Conta = require('./Conta')
 
 describe('Testes da classe conta', () => {
@@ -74,4 +84,91 @@ describe('Testes da classe conta', () => {
     expect(conta.getSaldo()).toBe(1000)
   })
 
-})
+  test("criar uma chave pix por cpf com sucesso", () => {
+    //setup
+    const conta = new Conta();
+
+    //acao
+    const operacao = conta.criarChavePix("40814360879", "CPF");
+
+    //verificacao
+    expect(operacao).toBe("Chave Pix por cpf criada com sucesso");
+    expect(conta.chavesPix.cpf).toBe("40814360879");
+
+    // remover conta da lista de contas
+    conta.destruir()
+  });
+
+  test("retornar mensagem de erro ao tentar cadastrar chave pix com cpf invalido", () => {
+    //setup
+    const conta = new Conta();
+
+    //verificacao
+    expect(() => conta.criarChavePix("124861", "CPF")).toThrow("Erro: CPF inválido");
+
+    // remover conta da lista de contas
+    conta.destruir()
+});
+
+  test("criar uma chave pix por email com sucesso", () => {
+    //setup
+    const conta = new Conta();
+
+    //acao
+    const operacao = conta.criarChavePix("analu@email.com", "EMAIL");
+
+    //verificacao
+    expect(operacao).toBe("Chave Pix por email criada com sucesso");
+    expect(conta.chavesPix.email).toBe("analu@email.com");
+
+    // remover conta da lista de contas
+    conta.destruir()
+  });
+
+  test("criar uma chave pix por telefone com sucesso", () => {
+    //setup
+    const conta = new Conta();
+
+    //acao
+    const operacao = conta.criarChavePix("11951639874", "TELEFONE");
+
+    //verificacao
+    expect(operacao).toBe("Chave Pix por telefone criada com sucesso");
+    expect(conta.chavesPix.telefone).toBe("11951639874");
+
+    // remover conta da lista de contas
+    conta.destruir()
+  });
+
+  /**
+   * TRANFERENCIA
+   * emissor = conta q esta enviando o dinheiro
+   * recepto = conta q está recebendo esse dinheiro
+   * agencia e conta do receptor
+   * metodo vai precisar de valor, agencia do Receptor e conta do Receptor
+   * valor valido
+   * saldo suficiente
+   * dados validos do receptor
+   */
+
+  test("retorna sucesso ao fazer uma transferencia com valor válido, saldo suficiente, dados validos", ()=>{
+    //setup
+    const contaEmissor = new Conta();
+    const contaReceptor = new Conta();
+
+    contaEmissor.criarConta("0001", "12345", 1000 )
+    contaReceptor.criarConta("0001", "78945", 500 )
+
+    //acao
+    const operacao = contaEmissor.transferir(100, "0001", "78945")
+
+    //verificacao
+    expect(operacao).toBe("Tranferencia realizada")
+    expect(contaEmissor.getSaldo()).toBe(900)
+    expect(contaReceptor.getSaldo()).toBe(600)
+
+    contaEmissor.destruir();
+    contaReceptor.destruir();
+
+  })
+});
